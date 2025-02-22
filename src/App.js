b18a76a65ce12b8a3ea060bbc252ee483c0e4d3c
KitBob <christiansawai0@gmail.com>
2025-02-21 19:43:47 -0500

    Updated Homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Navbar from './components/Navbar';
 import LandingPage from './pages/LandingPage';
 
@@ -9,36 +8,6 @@ function App() {
             <LandingPage />
         </div>
     );
-=======
-import React from "react";
-import "./index.css";
-
-function App() {
-  return (
-    <div className="homepage">
-      <header>
-        <div className="logo">
-          <img src="/logo.jpg" alt="IMAPA Logo" />
-        </div>
-        <nav>
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#events">Events</a></li>
-            <li><a href="#departments">Departments</a></li>
-          </ul>
-        </nav>
-      </header>
-
-      <section className="hero">
-        <div className="hero-text">
-          <h1>IMAPA USA-Canada</h1>
-          <p>Ikatan Mahasiswa Papua di USA dan Canada</p>
-        </div>
-      </section>
-    </div>
-  );
->>>>>>> eabc939 (Updated HomePage with UI)
 }
 
 export default App;
